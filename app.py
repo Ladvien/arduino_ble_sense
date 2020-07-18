58fa5567d71bb5eab99abe132204a0afc09abd78
C. Thomas Brittain <honeysucklehardware@gmail.com>
2020-07-18 14:26:51 -0500

    Finished refactor 1

--- app.py
+++ app.py
@@ -3,51 +3,51 @@ import os, sys
 import asyncio
 import platform
 from datetime import datetime
+from typing import Callable, Any
 
 from aioconsole import ainput
-from bleak import BleakClient
-import pandas as pd
-from typing import Callable, Any
+from bleak import BleakClient, discover
+
 
 root_path = os.environ["HOME"]
 output_file = f"{root_path}/Desktop/microphone_dump.csv"
-column_names = ["time", "micro_secs_since_last", "microphone_value"]
 
+selected_device = []
 
 class DataToFile:
-    def __init__(self, write_path, column_names):
-        self.path = write_path
-        self.column_names = column_names
 
-    def write_to_csv(self, data: tuple):
-        prev_leng = 0
-        for column in data:
-            if len(column) != prev_leng and prev_leng != 0:
-                raise Exception("Not all columns are the same length.")
-            prev_leng = len(column)
+    column_names = ["time", "delay", "data_value"]
+
+    def __init__(self, write_path):
+        self.path = write_path
+
+    def write_to_csv(self, times: [int], delays: [datetime], data_values: [Any]):
+
+        if len(set([len(times), len(delays), len(data_values)])) > 1:
+            raise Exception("Not all data lists are the same length.")
 
         with open(self.path, "a+") as f:
             if os.stat(self.path).st_size == 0:
                 print("Created file.")
                 f.write(",".join([str(name) for name in self.column_names]) + ",\n")
             else:
-                for row_idx in range(len(data[0])):
-                    row = ""
-                    for col_idx in range(len(data)):
-                        row += f"{data[col_idx][row_idx]},"
-                    row += "\n"
-                    f.write(row)
-                        # f.write(f"{timestamps[i]},{delays[i]},{data[i]},\n")
+                for i in range(len(data_values)):
+                    f.write(f"{times[i]},{delays[i]},{data_values[i]},\n")
+
+
 class Connection:
+    
+    client: BleakClient = None
+    
     def __init__(
         self,
-        client: BleakClient,
+        loop: asyncio.AbstractEventLoop,
         read_characteristic: str,
         write_characteristic: str,
         data_dump_handler: Callable[[str, Any], None],
         data_dump_size: int = 256,
     ):
-        self.client = client
+        self.loop = loop
         self.read_characteristic = read_characteristic
         self.write_characteristic = write_characteristic
         self.data_dump_handler = data_dump_handler
@@ -55,33 +55,75 @@ class Connection:
         self.last_packet_time = datetime.now()
         self.dump_size = data_dump_size
         self.connected = False
+        self.connected_device = None
 
         self.rx_data = []
         self.rx_timestamps = []
         self.rx_delays = []
 
-    def disconnect_callback(self, client):
+    def on_disconnect(self, client: BleakClient):
         self.connected = False
-        print(f"Disconnected from {client}!")
+        # Put code here to handle what happens on disconnet.
+        print(f"Disconnected from {self.connected_device.name}!")
 
     async def cleanup(self):
-        await self.client.stop_notify(read_characteristic)
-        await self.client.disconnect()
+        if self.client:
+            await self.client.stop_notify(read_characteristic)
+            await self.client.disconnect()
 
-    async def manager(self, client: BleakClient):
+    async def manager(self):
+        print("Starting connection manager.")
         while True:
-            if not self.connected:
-                try:
-                    await client.connect()
-                    self.connected = await client.is_connected()
-                    client.set_disconnected_callback(self.disconnect_callback)
-                    await client.start_notify(
-                        self.read_characteristic, self.notification_handler,
-                    )
-                    while True:
-                        await asyncio.sleep(15.0, loop=loop)
-                except Exception as e:
-                    print(e)
+            if self.client:
+                await self.connect()
+            else:
+                await self.select_device()
+                await asyncio.sleep(15.0, loop=loop)       
+
+    async def connect(self):
+        if self.connected:
+            return
+        try:
+            await self.client.connect()
+            self.connected = await self.client.is_connected()
+            if self.connected:
+                print(F"Connected to {self.connected_device.name}")
+                self.client.set_disconnected_callback(self.on_disconnect)
+                await self.client.start_notify(
+                    self.read_characteristic, self.notification_handler,
+                )
+                while True:
+                    await asyncio.sleep(15.0, loop=loop)
+            else:
+                print(f"Failed to connect to {self.connected_device.name}")
+        except Exception as e:
+            print(e)
+
+    async def select_device(self):
+        print("Bluetooh LE hardware warming up...")
+        await asyncio.sleep(2.0, loop=loop) # Wait for BLE to initialize.
+        devices = await discover()
+
+        print("Please select device: ")
+        for i, device in enumerate(devices):
+            print(f"{i}: {device.name}")
+
+        response = -1
+        while True:
+            response = await ainput("Select device: ")
+            try:
+                response = int(response.strip())
+            except:
+                print("Please make valid selection.")
+            
+            if response > -1 and response < len(devices):
+                break
+            else:
+                print("Please make valid selection.")
+
+        print(f"Connecting to {devices[response].name}")
+        self.connected_device = devices[response]
+        self.client = BleakClient(devices[response].address, loop=self.loop)
 
     def record_time_info(self):
         present_time = datetime.now()
@@ -98,20 +140,19 @@ class Connection:
         self.rx_data.append(int.from_bytes(data, byteorder="big"))
         self.record_time_info()
         if len(self.rx_data) >= self.dump_size:
-            data = (self.rx_data, self.rx_timestamps, self.rx_delays)
-            self.data_dump_handler(data)
+            self.data_dump_handler(self.rx_data, self.rx_timestamps, self.rx_delays)
             self.clear_lists()
 
 
 #############
 # Loops
 #############
-async def user_console(client: BleakClient, connection: Connection):
+async def user_console_manager(connection: Connection):
     while True:
-        if connection.connected:
+        if connection.client and connection.connected:
             input_str = await ainput("Enter string: ")
             bytes_to_send = bytearray(map(ord, input_str))
-            await client.write_gatt_char(write_characteristic, bytes_to_send)
+            await connection.client.write_gatt_char(write_characteristic, bytes_to_send)
             print(f"Sent: {input_str}")
         else:
             await asyncio.sleep(2.0, loop=loop)
@@ -119,36 +160,29 @@ async def user_console(client: BleakClient, connection: Connection):
 
 async def main():
     while True:
-        # YOUR CODE WOULD GO HERE.
+        # YOUR APP CODE WOULD GO HERE.
         await asyncio.sleep(5)
 
 
+
 #############
-# Main
+# App Main
 #############
 read_characteristic = "00001143-0000-1000-8000-00805f9b34fb"
 write_characteristic = "00001142-0000-1000-8000-00805f9b34fb"
 
 if __name__ == "__main__":
 
-    if sys.platform == "darwin":
-        # Mac.
-        address = "46BFEB38-910C-4490-962E-CD60E52D7AF1"
-    else:
-        # Windows or Linux.
-        address = "E6:38:7B:5E:A9:24"
-
     # Create the event loop.
     loop = asyncio.get_event_loop()
-    data_to_file = DataToFile(output_file, column_names)
-    # Create the Bluetooth LE object.
-    client = BleakClient(address, loop=loop)
+
+    data_to_file = DataToFile(output_file)
     connection = Connection(
-        client, read_characteristic, write_characteristic, data_to_file.write_to_csv
+        loop, read_characteristic, write_characteristic, data_to_file.write_to_csv
     )
     try:
-        asyncio.ensure_future(connection.manager(client))
-        asyncio.ensure_future(user_console(client, connection))
+        asyncio.ensure_future(connection.manager())
+        asyncio.ensure_future(user_console_manager(connection))
         asyncio.ensure_future(main())
         loop.run_forever()
     except KeyboardInterrupt:
